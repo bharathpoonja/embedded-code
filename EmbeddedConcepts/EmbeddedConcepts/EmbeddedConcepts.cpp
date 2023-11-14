100ed2b6a7b9116491b8498c57962f3bfff16385
Vibha <Vibha@LAPTOP-HKRR59IU>
2023-11-14 17:02:00 +0530

    Init

---
+++ EmbeddedConcepts/EmbeddedConcepts/EmbeddedConcepts.cpp
@@ -0,0 +1,12 @@
+﻿// EmbeddedConcepts.cpp : Defines the entry point for the application.
+//
+
+#include "EmbeddedConcepts.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
